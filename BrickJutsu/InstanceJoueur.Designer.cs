3fb43f12b1f0e20c5ed66ea922aa0e9283c20088
Elodie <Elodie@PC-ELODIE>
2017-12-07 13:20:58 +0100

    Ajout fonction alétoire pour le nombre

--- BrickJutsu/InstanceJoueur.Designer.cs
+++ BrickJutsu/InstanceJoueur.Designer.cs
@@ -30,26 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstanceJoueur));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Joueur1 = new System.Windows.Forms.TextBox();
+            this.Joueur = new System.Windows.Forms.TextBox();
             this.Joueurs = new MetroFramework.Controls.MetroTile();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultatj1 = new System.Windows.Forms.Label();
+            this.resultatj2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.resultatj2);
+            this.panel1.Controls.Add(this.resultatj1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Joueur1);
+            this.panel1.Controls.Add(this.Joueur);
             this.panel1.Controls.Add(this.Joueurs);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
@@ -57,6 +65,58 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(870, 500);
             this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(613, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 21);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(141, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Saisissez votre nom : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(534, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Saisissez votre nom : ";
+            // 
+            // Joueur1
+            // 
+            this.Joueur1.Location = new System.Drawing.Point(537, 201);
+            this.Joueur1.Name = "Joueur1";
+            this.Joueur1.Size = new System.Drawing.Size(195, 20);
+            this.Joueur1.TabIndex = 4;
+            // 
+            // Joueur
+            // 
+            this.Joueur.Location = new System.Drawing.Point(68, 201);
+            this.Joueur.Name = "Joueur";
+            this.Joueur.Size = new System.Drawing.Size(195, 20);
+            this.Joueur.TabIndex = 3;
             // 
             // Joueurs
             // 
@@ -90,55 +150,39 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // label3
             // 
-            this.textBox1.Location = new System.Drawing.Point(68, 201);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 20);
-            this.textBox1.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nombre : ";
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Location = new System.Drawing.Point(537, 201);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 20);
-            this.textBox2.TabIndex = 4;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nombre :";
             // 
-            // label1
+            // resultatj1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(534, 185);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Saisissez votre nom : ";
+            this.resultatj1.AutoSize = true;
+            this.resultatj1.Location = new System.Drawing.Point(125, 278);
+            this.resultatj1.Name = "resultatj1";
+            this.resultatj1.Size = new System.Drawing.Size(0, 13);
+            this.resultatj1.TabIndex = 11;
             // 
-            // label2
+            // resultatj2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 185);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Saisissez votre nom : ";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(141, 242);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(48, 21);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "OK";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(613, 242);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(48, 21);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "OK";
-            this.button4.UseVisualStyleBackColor = true;
+            this.resultatj2.AutoSize = true;
+            this.resultatj2.Location = new System.Drawing.Point(591, 278);
+            this.resultatj2.Name = "resultatj2";
+            this.resultatj2.Size = new System.Drawing.Size(0, 13);
+            this.resultatj2.TabIndex = 12;
             // 
             // InstanceJoueur
             // 
@@ -167,7 +211,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Joueur1;
+        private System.Windows.Forms.TextBox Joueur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultatj2;
+        private System.Windows.Forms.Label resultatj1;
     }
 }
--- BrickJutsu/InstanceJoueur.cs
+++ BrickJutsu/InstanceJoueur.cs
@@ -12,6 +12,9 @@ namespace BrickJutsu
 {
     public partial class InstanceJoueur : MetroFramework.Forms.MetroForm
     {
+        private int nbJ1;
+        private int nbJ2;
+        
         public InstanceJoueur()
         {
             InitializeComponent();
@@ -22,6 +25,16 @@ namespace BrickJutsu
             System.Threading.Thread monthread = new System.Threading.Thread(new System.Threading.ThreadStart(jouer));
             monthread.Start();
             this.Close();
+            Joueur j = new Joueur(Joueur.ToString());
+            Joueur j1 = new Joueur(Joueur1.ToString());
+            if (this.nbJ1 > this.nbJ2)
+            {
+                MessageBox.Show(j + " commence la partie ! ");
+            }
+            else
+            {
+                MessageBox.Show(j1 + " commence la partie ! ");
+            }
         }
 
         public static void jouer()
@@ -39,6 +52,22 @@ namespace BrickJutsu
         public static void retour()
         {
             Application.Run(new Accueil());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random alea = new Random();
+            int dé1 = alea.Next(1, 7);
+            resultatj1.Text = dé1.ToString();
+            this.nbJ1 = dé1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random alea = new Random();
+            int dé2 = alea.Next(1, 7);
+            resultatj2.Text = dé2.ToString();
+            this.nbJ2 = dé2;
         }
     }
 }
