9e68d28841e9aaf575a90f85a165cf741f2dbd34
Kevin <Kevin@fxlan.com>
2017-12-08 09:25:51 +0100

    accueil enfin propre

--- BrickJutsu/Accueil.cs
+++ BrickJutsu/Accueil.cs
@@ -10,12 +10,47 @@ using System.Windows.Forms;
 
 namespace BrickJutsu
 {
-    public partial class Accueil : Form
+    public partial class Accueil : MetroFramework.Forms.MetroForm
     {
         public Accueil()
         {
             InitializeComponent();
-            //test
+        }
+
+        //plateau
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread monthread = new System.Threading.Thread(new System.Threading.ThreadStart(jouer));
+            monthread.Start();
+            this.Close();
+        }
+
+        public static void jouer()
+        {
+            Application.Run(new InstanceJoueur());
+        }
+
+        //gestion collection
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread monthread = new System.Threading.Thread(new System.Threading.ThreadStart(gestionCollection));
+            monthread.Start();
+            this.Close();
+        }
+
+        public static void gestionCollection()
+        {
+            Application.Run(new VoirLesCartes());
+        }
+         
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            DialogResult retour = MessageBox.Show("Voulez vraiment quitter le jeu ? ", "Attention !", MessageBoxButtons.YesNoCancel);
+            if (retour == DialogResult.Yes)
+            {
+                this.Close();
+            }
+
         }
     }
 }
